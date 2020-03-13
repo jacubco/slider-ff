8bbc75d3b261c30532dc0f1d1ad6bec675c6fe62
jep <jep@mbp.local>
2020-03-13 16:07:55 +0100

    Add modal for read more

--- scripts.js
+++ scripts.js
@@ -31,7 +31,7 @@ var view = (function() {
       testimonial = document.querySelector('.testimonial');
       var markup = createMarkup(i);
       testimonial.innerHTML = markup;
-      listenForQuoteToggle()  
+      // listenForQuoteToggle()
     }
 
   }
@@ -115,17 +115,50 @@ function createMarkup(i) {
     <div class="testimonial__container fade">
       <img class="testimonial__image" src=${testimonials[i].imgURL} alt="">
       <div class="testimonial__quote testimonial__quote--short fade">${testimonials[i].quoteShort}</div>
-      <div class="testimonial__read-more fade ">Read more</div>
+      <div class="testimonial__read-more fade" data-toggle="modal" data-target="#myModal">Read more</div>
       <div class="testimonial__quote testimonial__quote--long  fade no-show">${testimonials[i].quoteLong}</div><div class="testimonial__read-more testimonial__read-more--less fade no-show">Read less</div>
       <div class="testimonial__id">
         <div class="testimonial__name">${testimonials[i].name}</div>
         <div class="testimonial__title">${testimonials[i].title}</div>
       </div>
     </div>
+
+    <!-- The Modal -->
+    <div class="modal" id="myModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <h4 class="modal-title lead">${testimonials[i].quoteLong}</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+
+          <!-- Modal body -->
+          
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-6">
+                <img class="testimonial__image rounded-circle" src=${testimonials[i].imgURL} alt="">
+              </div>
+              <div class="col-6 text-left pt-2">
+                <div class="font-weight-bold">${testimonials[i].name}</div>
+                <div class="font-weight-light">${testimonials[i].title}</div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Modal footer -->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
   `
   return markup
 }
-
 
 // Create and add dots (representing available slides)
 function createIndicators() {
@@ -168,17 +201,17 @@ function getID(eventTarget) {
 /////////////////////////// READ MORE
 
 // Listen for click on read more
-function listenForQuoteToggle() {
-  document.querySelectorAll('.testimonial__read-more').forEach(item => item.addEventListener('click', toggleQuote));
-}
+// function listenForQuoteToggle() {
+//   document.querySelectorAll('.testimonial__read-more').forEach(item => item.addEventListener('click', toggleQuote));
+// }
 
-// Toggle read more
-function toggleQuote() {
-  document.querySelector('.testimonial__quote--short').classList.toggle('no-show')
-  document.querySelector('.testimonial__quote--long').classList.toggle('no-show')
+// // Toggle read more
+// function toggleQuote() {
+//   document.querySelector('.testimonial__quote--short').classList.toggle('no-show')
+//   document.querySelector('.testimonial__quote--long').classList.toggle('no-show')
 
-  document.querySelector('.testimonial__read-more').classList.toggle('no-show')
+//   document.querySelector('.testimonial__read-more').classList.toggle('no-show')
 
-  document.querySelector('.testimonial__read-more--less').classList.toggle('no-show')
+//   document.querySelector('.testimonial__read-more--less').classList.toggle('no-show')
 
-}
+// }
