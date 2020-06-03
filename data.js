5ab72daf9751c8dcaa3e35efa7da7a6b6647609a
jep <jep@mbp.local>
2020-06-03 21:46:48 +0200

    Add titleShort category

--- data.js
+++ data.js
@@ -2,6 +2,7 @@ const testimonials = [
   {
     name: 'Mikko Tolonen',
     title: 'Professor in digital humanities at the Faculty of Arts at the University of Helsinki',
+    titleShort:'Uni Helsinki',
     quoteShort: '"Lorem ipsum dolor sit amet consectetur adipisicing elit."',
     quoteLong: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, distinctio in vitae cum dolor sed repudiandae saepe fuga quisquam neque nesciunt optio mollitia, pariatur, quam eligendi porro tempora asperiores modi."',
     imgURL: 'http://placekitten.com/150/150'
@@ -10,6 +11,7 @@ const testimonials = [
   {
     name: 'Fictional Person 1',
     title: 'Professor in whatsoever at the Faculty of something at the University of Nowhere',
+    titleShort:'Uni Helsinki',
     quoteShort: '"Hooooraaay!"',
     quoteLong: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, distinctio in vitae cum dolor sed repudiandae saepe fuga quisquam neque nesciunt optio mollitia, pariatur, quam eligendi porro tempora asperiores modi."',
     imgURL: 'http://placekitten.com/150/150'
@@ -18,6 +20,7 @@ const testimonials = [
   {
     name: 'Fictional Person 2',
     title: 'Professor in whatsoever at the Faculty of something at the University of Nowhere',
+    titleShort:'Uni Helsinki',
     quoteShort: '"Very gooooooood"',
     quoteLong: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, distinctio in vitae cum dolor sed repudiandae saepe fuga quisquam neque nesciunt optio mollitia, pariatur, quam eligendi porro tempora asperiores modi."',
     imgURL: 'http://placekitten.com/150/150'
@@ -26,6 +29,7 @@ const testimonials = [
   {
     name: 'Fictional Person 3',
     title: 'Professor in whatsoever at the Faculty of something at the University of Nowhere',
+    titleShort:'Uni Helsinki',
     quoteShort: '"blablbalblalbablblablablalabablablalbalbalblalblallba"',
     quoteLong: '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, distinctio in vitae cum dolor sed repudiandae saepe fuga quisquam neque nesciunt optio mollitia, pariatur, quam eligendi porro tempora asperiores modi."',
     imgURL: 'http://placekitten.com/150/150'
